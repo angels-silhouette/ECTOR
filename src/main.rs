ceba09d4fbc8c5728a6aa0c6804ad2cbf02761e3
Angel's Silhouette <silhouette@LI-PC-LINUX.home>
2024-07-06 15:40:05 +1000

    uwu

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
-use rand::random;
-use std::io::stdin;
+use std::env;
 
-fn main() {}
+fn encrypt(key: u128, string: String) -> String {
+    
+
+    return output;
+}
+
+fn main() {
+    let mut working_dir: String = String::from("");
+    let mut switches: Vec<char> = Vec::new();
+    let mut flags: Vec<String> = Vec::new();
+    let mut arguments: String = String::from("");
+
+    {
+        let args: Vec<String> = env::args().collect();
+
+        for arg in args.iter().enumerate() {
+            if arg.0 == 0 {
+                working_dir = arg.1.clone();
+            } else if arg.1.starts_with("--") {
+                println!("This program does not accept any flags");
+            } else if arg.1.starts_with("-") {
+                for item in arg.1.chars() {
+                    switches.push(item.clone());
+                }
+            } else {
+                arguments = arg.1.clone();
+            }
+        }
+    }
+
+    if switches.contains('e') {
+        
+    }
+}
